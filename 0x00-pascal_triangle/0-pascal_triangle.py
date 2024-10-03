bab25bbcf7a7c5baa44fe0ad3f83fb38c57bc3e1
abdallahade1 <abdoadelshabaan@gmail.com>
2024-10-03 19:04:50 +0000

    Q1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
   """
   Returns a list of integers
@@ -20,7 +21,6 @@ def pascal_triangle(n):
       new_row = [1]
       for j in range(1, i):
         new_row.append(prev_row[j-1] + prev_row[j])
-
 
       new_row.append(1)
       triangle.append(new_row)
